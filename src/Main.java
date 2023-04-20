f5094e90d0c85273e0d4425c46b9ab9721a52bea
Professional <ustu1982@mail.ru>
2023-04-20 21:17:50 +0500

    homwork2

--- src/Main.java
+++ src/Main.java
@@ -33,5 +33,25 @@ public class Main {
         System.out.println(frog);
         frog=frog+4;
         System.out.println(frog);
+
+        var boxerOne=78.2;
+        var boxerTwo=82.7;
+        var allBoxer=boxerOne+boxerTwo;
+        System.out.println("Общий вес боксеров "+allBoxer+"кг ");
+        var differenceBoxer=boxerOne-boxerTwo;
+        System.out.println("Разница в весе "+differenceBoxer+ "кг");
+        var difBoxer=boxerTwo-boxerOne;
+        System.out.println("Разница весов боксеров " +difBoxer+ "кг");
+        var diBoxer=boxerTwo % boxerOne;
+        System.out.println("Разница весов "+diBoxer+"кг ");
+
+        var allHour=640;
+        var hourOne=8;
+        var staffAll=allHour / hourOne;
+        System.out.println("Всего работников в компании " +staffAll+ "человек");
+
+        var staffPlus=staffAll+94;
+        var hourPlus=staffPlus*hourOne;
+        System.out.println("Если в компании работает "+staffPlus+ "человек, то всего "+hourPlus+ "часов работы может быть поделено между сотрудниками");
     }
 }
